a44786dbebde401ef6028d3817a182a4feff026b
Erlend Eelmets <erlend.eelmets@gmail.com>
2022-09-04 11:07:59 +0300

    Edits

---
+++ popup/popup.js
@@ -0,0 +1,37 @@
+function saveOptions(e) {
+  e.preventDefault();
+  browser.storage.local.set({
+    scorePost: document.querySelector("#scores_post").checked,
+    scoreComment: document.querySelector("#scores_comment").checked,
+    scoreUser: document.querySelector("#scores_user").checked,
+    arrows: document.querySelector("#arrows").checked
+  });
+  browser.tabs.reload();
+}
+
+function restoreOptions() {
+
+  function setCurrentChoice(result) {
+    document.querySelector("#scores_post").checked = typeof result.scorePost !== 'undefined' ? result.scorePost: true;
+    document.querySelector("#scores_comment").checked = typeof result.scoreComment !== 'undefined' ? result.scoreComment: true;
+    document.querySelector("#scores_user").checked = typeof result.scoreUser !== 'undefined' ? result.scoreUser: true;
+    document.querySelector("#arrows").checked = typeof result.arrows !== 'undefined' ? result.arrows: true;
+  }
+
+  function onError(error) {
+    console.log(`Error: ${error}`);
+  }
+
+  var getting = browser.storage.local.get(['scorePost', 'scoreComment', 'scoreUser', 'arrows']);
+  getting.then(setCurrentChoice, onError);
+}
+
+function onError(error) {
+  console.log(`Error: ${error}`);
+};
+
+
+
+document.querySelector("#reddit-karma-blocker-logo").src = browser.runtime.getURL("icons/karma.png");
+document.addEventListener("DOMContentLoaded", restoreOptions);
+document.querySelector("form").addEventListener("submit", saveOptions);
