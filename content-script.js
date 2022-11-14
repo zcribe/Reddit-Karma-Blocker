e2adc1c582b3137642b629ff43ac74026d31cceb
Erlend Eelmets <erlend.eelmets@gmail.com>
2022-11-14 19:42:20 +0200

    Rewrite tags, rework the UI, add awards hiding

--- content-script.js
+++ content-script.js
@@ -1,28 +1,14 @@
 var TAGS = [
-  ".score",
-  "[data-test-id=post-content] > div:nth-of-type(1) > div:nth-of-type(1) > div:nth-of-type(1)",
-  "*[id^=vote-arrows-] > div",
-  "[data-click-id=background] > div:nth-of-type(4) > div:nth-of-type(1) > div:nth-of-type(4) > div:nth-of-type(1)",
-  "#email-collection-tooltip-id > span:nth-of-type(1) > span:nth-of-type(2) > span:nth-of-type(1)",
-  ".userkarma",
+
 ];
 
 var COMMENT_TAGS = [
   ".score",
-  "[data-test-id=post-content] > div:nth-of-type(1) > div:nth-of-type(1) > div:nth-of-type(1)",
-  "*[id^=vote-arrows-] > div",
-  "[data-click-id=background] > div:nth-of-type(4) > div:nth-of-type(1) > div:nth-of-type(4) > div:nth-of-type(1)",
-  "#email-collection-tooltip-id > span:nth-of-type(1) > span:nth-of-type(2) > span:nth-of-type(1)",
-  ".userkarma",
 ];
 
 var POST_TAGS = [
-  ".score",
-  "[data-test-id=post-content] > div:nth-of-type(1) > div:nth-of-type(1) > div:nth-of-type(1)",
   "*[id^=vote-arrows-] > div",
-  "[data-click-id=background] > div:nth-of-type(4) > div:nth-of-type(1) > div:nth-of-type(4) > div:nth-of-type(1)",
-  "#email-collection-tooltip-id > span:nth-of-type(1) > span:nth-of-type(2) > span:nth-of-type(1)",
-  ".userkarma",
+  "#siteTable > .thing > .midcol > .score",
 ];
 
 var USER_TAGS = [
@@ -31,11 +17,26 @@ var USER_TAGS = [
   ".userkarma",
 ];
 
-var ARROWS_TAGS = ["*[id^=vote-arrows-]"];
+var AWARD_TAGS = [
+  ".awardings-bar",
+  "*[id^=PostAwardBadges--]",
+  "*[id^=CommentAwardBadges--]",
+  "[data-test-id^=post-content] > div:nth-of-type(2) > div:nth-of-type(1) > div:nth-of-type(3)",
+  "[data-click-id^=background] > div:nth-of-type(1) > div:nth-of-type(1) > div:nth-of-type(1)> div:nth-of-type(2)",
+];
 
+var ARROWS_TAGS = [
+  ".arrow",
+  ".arrow-up",
+  ".arrow-down",
+  ".icon-upvote",
+  "*[id^=upvote-button-]",
+  "*[data-click-id^=upvote]",
+  "*[data-click-id^=downvote]"
+];
 
 function removeContent(settings) {
-  var tags = []
+  var tags = [];
   if (settings.scorePost) {
     tags = POST_TAGS;
   }
@@ -46,6 +47,10 @@ function removeContent(settings) {
 
   if (settings.scoreUser) {
     tags = tags.concat(USER_TAGS);
+  }
+
+  if (settings.awards) {
+    tags = tags.concat(AWARD_TAGS);
   }
 
   if (settings.arrows) {
@@ -68,17 +73,22 @@ function removeKarma() {
     "scoreComment",
     "scoreUser",
     "arrows",
+    "awards",
   ]);
 
-    settings.then(function (result){
-      if (Object.keys(result).length !== 0) {
-        removeContent(result)
-      } else {
-        removeContent({'scorePost': true, 'scoreComment': true, 'scoreUser': true, 'arrows': true})
-      }
-      
-    }, onError);
-  
+  settings.then(function (result) {
+    if (Object.keys(result).length !== 0) {
+      removeContent(result);
+    } else {
+      removeContent({
+        scorePost: true,
+        scoreComment: true,
+        scoreUser: true,
+        arrows: true,
+        awards: true,
+      });
+    }
+  }, onError);
 }
 
 removeKarma();
@@ -88,5 +98,5 @@ window.addEventListener("scroll", (_event) => {
 });
 
 window.addEventListener("DOMContentLoaded", (_event) => {
-    removeKarma();
+  removeKarma();
 });
--- popup/popup-chrome.js
+++ popup/popup-chrome.js
@@ -4,7 +4,8 @@ function saveOptions(e) {
     scorePost: document.querySelector("#scores_post").checked,
     scoreComment: document.querySelector("#scores_comment").checked,
     scoreUser: document.querySelector("#scores_user").checked,
-    arrows: document.querySelector("#arrows").checked
+    arrows: document.querySelector("#arrows").checked,
+    awards: document.querySelector("#awards").checked
   });
   chrome.tabs.reload();
 }
@@ -16,13 +17,14 @@ function restoreOptions() {
     document.querySelector("#scores_comment").checked = typeof result.scoreComment !== 'undefined' ? result.scoreComment: true;
     document.querySelector("#scores_user").checked = typeof result.scoreUser !== 'undefined' ? result.scoreUser: true;
     document.querySelector("#arrows").checked = typeof result.arrows !== 'undefined' ? result.arrows: true;
+    document.querySelector("#awards").checked = typeof result.awards !== 'undefined' ? result.awards: true;
   }
 
   function onError(error) {
     console.log(`Error: ${error}`);
   }
 
-  var getting = chrome.storage.local.get(['scorePost', 'scoreComment', 'scoreUser', 'arrows']);
+  var getting = chrome.storage.local.get(['scorePost', 'scoreComment', 'scoreUser', 'arrows', 'awards']);
   getting.then(setCurrentChoice, onError);
 }
 
